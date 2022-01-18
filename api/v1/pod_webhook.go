79fc965fc1c7c8646dba014b52f855a1f656ec16
Jack Du <jack_du@epam.com>
2022-01-18 22:58:01 +0800

    first commit

---
+++ api/v1/pod_webhook.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2022 jack.du.
+*/
+
+package v1
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// log is for logging in this package.
+var podlog = logf.Log.WithName("PodResourcesMutate")
+
+type PodResourcesMutate struct {
+	Client  client.Client
+	decoder *admission.Decoder
+}
+
+//+kubebuilder:webhook:path=/mutate-core-v1-pod,mutating=true,failurePolicy=fail,sideEffects=None,groups=core,resources=pods,verbs=create;update,versions=v1,name=mpod.kb.io,admissionReviewVersions={v1,v1beta1}
+
+func (a *PodResourcesMutate) Handle(ctx context.Context, req admission.Request) admission.Response {
+	pod := &corev1.Pod{}
+	err := a.decoder.Decode(req, pod)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	// mutate the fields in pod
+	pod.Labels["test"] = "test1"
+	podlog.Info("add labels test")
+	marshaledPod, err := json.Marshal(pod)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+}
+
+// InjectDecoder injects the decoder.
+func (v *PodResourcesMutate) InjectDecoder(d *admission.Decoder) error {
+	v.decoder = d
+	return nil
+}
